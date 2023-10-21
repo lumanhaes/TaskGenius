5264ae28dbcd604039e837ae4315dc543f1ce5b6
lucas <ramosmanhaes@outlook.com>
2023-10-21 16:00:16 -0300

    Adicionando estrutura do projeto

---
+++ src/test/java/br/com/taskgenius/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taskgenius.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
